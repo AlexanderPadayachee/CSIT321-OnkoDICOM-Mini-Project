ea2df4b7f3a7a8734111906f624955b4c44efe99
as565 <50353655+as565@users.noreply.github.com>
2022-05-13 12:55:24 +1000

    Test
    trying something out

---
+++ test/main.py
@@ -0,0 +1,25 @@
+import os
+import warnings
+import sys
+import platform
+import logging
+import PySide6
+from Controller.Controller import Controller
+
+
+if __name__ == "__main__":
+    rootDir = sys.argv[0]
+    logging.info("startup success")
+
+
+    ## GUI setup
+    app = PySide6.QtWidgets.QApplication(sys.argv)
+
+    mainController = Controller(rootDir)
+    logging.info("GUI init Successful")
+
+    mainController.showWindow()
+    app.exec()
+    logging.info("GUI exec Successful")
+
+    sys.exit()
