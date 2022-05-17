c5edf5805ef93ddac6c05e29b9de6dbb4f906b50
as565 <50353655+as565@users.noreply.github.com>
2022-05-17 23:50:02 +1000

    CI

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ import PySide6
 from Controller.Controller import Controller
 
 
-if __name__ == "__main__":
+# I did this to use pytest/CI on it, hope it's ok - Allen
+def main():
     rootDir = sys.argv[0]
     logging.info("startup success")
 
-
-    ## GUI setup
+    # GUI setup
     app = PySide6.QtWidgets.QApplication(sys.argv)
 
     mainController = Controller(rootDir)
@@ -21,5 +21,9 @@ if __name__ == "__main__":
     mainController.showWindow()
     app.exec()
     logging.info("GUI exec Successful")
+
+
+if __name__ == "__main__":
+    main()
 
     sys.exit()
---
+++ test/test_code.py
@@ -0,0 +1,14 @@
+import pytest
+import os
+import sys
+import main
+from Controller.Controller import Controller
+
+
+def test_open():
+    test_app = main.main()
+
+
+
+def test_directory():
+    e = 2
