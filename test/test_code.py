78ee16b122d4948d9a5ef8a5b5fddf59500bedff
as565 <50353655+as565@users.noreply.github.com>
2022-05-19 14:56:49 +1000

    unit test

--- test/test_code.py
+++ test/test_code.py
@@ -1,16 +1,21 @@
 import pytest
 import os
 import sys
+import PySide6
 
 from View.View import View
 from Model.Model import Model
 from Controller.Controller import Controller
 
 
-def test_app(qtbot):
-    test_app = View
-    qtbot.addWidget(test_app)
-    return test_app
+def test_app():
+    test_dir = sys.argv[0]
+    test_application = PySide6.QtWidgets.QApplication(sys.argv)
+
+    test_controller = Controller(test_dir)
+
+    test_controller.showWindow()
+    test_application.exec()
 
 
 
