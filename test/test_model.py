d020cbd7045689a2903805f9f0caaea8dd027909
as565 <50353655+as565@users.noreply.github.com>
2022-05-23 18:47:21 +1000

    Update test_model.py

--- test/test_model.py
+++ test/test_model.py
@@ -1,21 +1,21 @@
 import pytest
 import os
 
-from Model.Model import open_dicom, dicom_to_image
+from Model.Model import Model
 
 
 def test_open_dicom():
     path = 'img/Pat01'
     full_path = os.path.join(os.getcwd(), path)
 
-    assert open_dicom(full_path)
+    assert Model.open_dicom(full_path)
 
 
 def test_dicom_to_image():
     path = 'img/Pat01'
     full_path = os.path.join(os.getcwd(), path)
 
-    test_data = open_dicom(full_path)
+    test_data = Model.open_dicom(full_path)
     test_dcm_data = test_data[0]
 
-    assert dicom_to_image(test_dcm_data)
+    assert Model.dicom_to_image(test_dcm_data)
