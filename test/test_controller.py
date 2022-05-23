e165769c959fa494f9fb601984b3e93fea2ea3a1
as565 <50353655+as565@users.noreply.github.com>
2022-05-23 12:36:47 +1000

    Update test_controller.py

--- test/test_controller.py
+++ test/test_controller.py
@@ -9,9 +9,6 @@ from Controller.Controller import Controller
 def test_controller():
     test_dir = sys.argv[0]
 
-    # core dump if this is not in the code
-    test_application = PySide6.QtWidgets.QApplication(sys.argv)
-
     assert Controller(test_dir)
 
 
