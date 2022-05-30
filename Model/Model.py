140d4222dd5d86d1419521ccf694f170e7d110c5
Alex <alexander.padayachee@gmail.com>
2022-05-30 18:18:03 +1000

    MVC refactoring
    Part of the way done to refactoring the code. It is functional in its current state, but not optimized

--- Model/Model.py
+++ Model/Model.py
@@ -9,40 +9,6 @@ import numpy as np
 class Model:
     def __init__(self, root_dir=None):
         self.root_dir = root_dir
-
-    @staticmethod
-    def open_dicom(directory):
-        str_input = directory + "/*.dcm"
-
-        dir_list = glob.glob(str_input)
-        temp_dcm_array = []
-        temp_misc_array = []
-        for i in dir_list:
-            ds = dcmread(i, force=True)
-            try:
-                temp = ds.get_item((0x0020, 0x1041)).value
-                temp_dcm_array.append(ds)
-            except:
-                temp_misc_array.append(ds)
-            # print(ds.get_item((0x0020,0x1041)).value)
-        Model.sort_dicom(temp_dcm_array)
-        return [temp_dcm_array, temp_misc_array]
-
-    @staticmethod
-    def sort_dicom(array):
-        array.sort(key=lambda x: float(x.get_item((0x0020, 0x1041)).value), reverse=True)
-        return array
-
-    @staticmethod
-    def dicom_to_image(dicom):
-        images = []
-        for i in dicom:
-            try:
-                arr = i.pixel_array.astype(float)
-                rescaled_im = (np.maximum(arr, 0) / arr.max()) * 255
-                final_image = np.uint8(rescaled_im)
-                patient_image = Image.fromarray(final_image)
-                images.append(patient_image)
-            except logging:
-                logging.info("Importing DICOM Files without pixel data")
-        return images
+        self.dcm_data = []  # DICOM files that contain pixel data are stored here as pydicom objects
+        self.dcm_misc = []  # DICOM files that contain no pixel data are stored here as pydicom objects
+        self.images = []  # DICOM images are stored here as PIL objects
--- main.py
+++ main.py
@@ -7,18 +7,19 @@ import PySide6
 from Controller.Controller import Controller
 
 if __name__ == "__main__":
-
+    log = logging.getLogger()
     # logging setup
     for handler in logging.root.handlers[:]:
         logging.root.removeHandler(handler)
-
-    logging.basicConfig(filename="OnkoLog.log", format="%(asctime)s %(message)s", datefmt="%m/%d/%Y %I:%M:%S %p")
+    logging.getLogger().setLevel(logging.DEBUG)
+    logging.basicConfig(filename="OnkoLog.log", format="%(asctime)s %(levelname)s %(message)s", datefmt="%m/%d/%Y %I:%M:%S %p")
 
     # ROOT DIRECTORY SETUP (not used in code, but is nice to have)
     root_dir = sys.argv[0]
     logging.info("startup success")
 
     # GUI setup
+    #
     app = PySide6.QtWidgets.QApplication(sys.argv)
 
     main_controller = Controller(root_dir)
