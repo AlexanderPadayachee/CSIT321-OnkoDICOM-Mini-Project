e25de6031892343f9dd22e5aad5eec9ad7a97bff
as565 <50353655+as565@users.noreply.github.com>
2022-06-02 01:51:22 +1000

    Update test_setup.py

--- test/test_setup.py
+++ test/test_setup.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 
 from Controller.Setup import *
@@ -8,7 +10,7 @@ def test_log_setup():
     Test to make sure that if the right
     parameters are met that 1 is returned
     """
-    assert logSetup('TestOnkoLog.log') == 1
+    assert logSetup('OnkoLog.log') == 1
 
 
 
