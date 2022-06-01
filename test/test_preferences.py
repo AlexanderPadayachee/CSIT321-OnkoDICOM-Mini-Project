6b7a278d592632aa5b164f407bd1704d57be4a67
as565 <50353655+as565@users.noreply.github.com>
2022-06-02 01:06:41 +1000

    Update test_preferences.py
    getData and comments

--- test/test_preferences.py
+++ test/test_preferences.py
@@ -4,12 +4,21 @@ import os
 from View.Preferences import *
 
 
-def test_home_address_not_current_directory():
+def test_home_address_fail():
+    """
+    Test to ensure that the getHomeAddress method
+    only outputs the home directory of the user
+    """
     test_path = os.getcwd()
     assert str(getHomeAddress()) != test_path
 
 
 def test_get_home_address():
+    """
+    Test to ensure that getHomeAddress returns
+    the home directory as per the os of the user
+    (e.g. 'C:/Users/alex')
+    """
     test_path = os.path.expanduser('~')
     assert str(getHomeAddress()) == test_path
 
@@ -22,5 +31,15 @@ def test_insert_data():
     insertData(1, 1)
 
 
+def test_get_data_exists():
+    """
+    Test to see if something exists within getData
+    """
+    assert getData() != [(0, 0)]
+
+
 def test_get_data():
+    """
+    Test to see if getData has an output
+    """
     assert getData() == [(1, 1)]
